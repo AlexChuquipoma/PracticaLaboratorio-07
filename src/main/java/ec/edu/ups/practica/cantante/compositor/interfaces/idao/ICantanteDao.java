252929378bdb020d19516f72349813fa529964c0
Daniel_Fabricio Nieves_Tacuri <fabricionieves2004@gmail.com>
2023-07-22 21:48:00 -0500

    Se agrego Al ICantanteDao y ICompositorDao Nuevos Metodos

--- src/main/java/ec/edu/ups/practica/cantante/compositor/interfaces/idao/ICantanteDao.java
+++ src/main/java/ec/edu/ups/practica/cantante/compositor/interfaces/idao/ICantanteDao.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ec.edu.ups.practica.cantante.compositor.interfaces.idao;
+
 import ec.edu.ups.practica.cantante.compositor.interfaces.modelo.Cantante;
 import ec.edu.ups.practica.cantante.compositor.interfaces.modelo.Disco;
 import java.util.List;
@@ -12,11 +13,16 @@ import java.util.List;
  * @author Usuario
  */
 public interface ICantanteDao {
-    
+
     public void create(Cantante cantante);
+
     public Cantante read(int id);
+
     public void update(Cantante cantante);
-    public void delete(Cantante cantante);  
+
+    public void delete(Cantante cantante);
+
     public Cantante buscarPorNombreDeDisco(String valor);
+
     public List<Cantante> findAll();
 }
--- src/main/java/ec/edu/ups/practica/cantante/compositor/interfaces/idao/ICompositorDao.java
+++ src/main/java/ec/edu/ups/practica/cantante/compositor/interfaces/idao/ICompositorDao.java
@@ -12,12 +12,17 @@ import java.util.List;
  * @author Usuario
  */
 public interface ICompositorDao {
-    
+
     public void create(Compositor compositor);
+
     public Compositor read(int codigo);
+
     public void update(Compositor compositor);
-    public void delete(Compositor compostior);   
+
+    public void delete(Compositor compostior);
+
     public Compositor buscarPorTituloDeCancion(String valor);
+
     public List<Compositor> findAll();
-    
+
 }
